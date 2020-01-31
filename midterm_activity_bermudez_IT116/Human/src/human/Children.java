75d1aa997405b8953d5d33fdabe62bf61dea60ad
Mario260599 <57735731+Mario260599@users.noreply.github.com>
2020-01-31 09:00:06 +0800

    Delete Children.java

--- midterm_activity_bermudez_IT116/Human/src/human/Children.java
+++
@@ -1,9 +0,0 @@
-
-package human;
-
-public class Children extends Human {
-    public int intellegience_rating = 150 ;
-    public int mood_rating = 7;
-    public String[] genetic_disease = {"Heart Disease", "HBP"};
-    public int wealth_rating = 102937;
-}
